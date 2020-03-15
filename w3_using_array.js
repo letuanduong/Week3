1977f5c192bbd4aa3abba1bde5e9dc45b996ec81
lemanhtuan <letuanmanh86@gmail.com>
2020-03-15 22:08:37 +0700

    thư viện

---
+++ w3_using_array.js
@@ -0,0 +1,70 @@
+function CheckString(str) {
+    switch (str) {
+        case 'Q' : return 1;
+        case 'W' : return 1;
+        case 'E' : return 1;
+        case 'R' : return 1;
+        case 'T' : return 1;
+        case 'Y' : return 1;
+        case 'U' : return 1;
+        case 'I' : return 1;
+        case 'O' : return 1;
+        case 'P' : return 1;
+        case 'A' : return 1;
+        case 'S' : return 1;
+        case 'D' : return 1;
+        case 'F' : return 1;
+        case 'G' : return 1;
+        case 'H' : return 1;
+        case 'J' : return 1;
+        case 'K' : return 1;
+        case 'L' : return 1;
+        case 'Z' : return 1;
+        case 'X' : return 1;
+        case 'C' : return 1;
+        case 'V' : return 1;
+        case 'B' : return 1;
+        case 'N' : return 1;
+        case 'M' : return 1;
+
+        case 'q' : return 0;
+        case 'w' : return 0;
+        case 'e' : return 0;
+        case 'r' : return 0;
+        case 't' : return 0;
+        case 'y' : return 0;
+        case 'u' : return 0;
+        case 'i' : return 0;
+        case 'o' : return 0;
+        case 'p' : return 0;
+        case 'a' : return 0;
+        case 's' : return 0;
+        case 'd' : return 0;
+        case 'f' : return 0;
+        case 'g' : return 0;
+        case 'h' : return 0;
+        case 'j' : return 0;
+        case 'k' : return 0;
+        case 'l' : return 0;
+        case 'z' : return 0;
+        case 'x' : return 0;
+        case 'c' : return 0;
+        case 'v' : return 0;
+        case 'b' : return 0;
+        case 'n' : return 0;
+        case 'm' : return 0;
+
+        case '0' : return 2;
+        case '1' : return 2;
+        case '2' : return 2;
+        case '3' : return 2;
+        case '4' : return 2;
+        case '5' : return 2;
+        case '6' : return 2;
+        case '7' : return 2;
+        case '8' : return 2;
+        case '9' : return 2;
+        default:
+            break;
+    }
+}
